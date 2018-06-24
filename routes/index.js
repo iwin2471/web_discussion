962c54fe6e5719911300ddc699a9b5e56dd57b6f
iwin2471 <iwin12372@gmail.com>
2018-06-24 13:04:54 +0900

    add: policy

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 let index = router => {
-  router.get("/", (req, res, next) => {
-    if (!req.isAuthenticated()) return res.render("M_FirstandLogin");
-    else res.redirect("/discuss");
-    next();
-  });
+  router
+    .get("/", (req, res, next) => {
+      if (!req.isAuthenticated()) return res.render("M_FirstandLogin");
+      else res.redirect("/discuss");
+      next();
+    })
+    .get("/policy", (req, res) => {
+      return res.render("summary");
+    });
 
   return router;
 };
